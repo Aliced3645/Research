8a41312452abc8b38851e571e03118b59660491d
Shu Zhang <shu_zhang1989@yahoo.com.cn>
2013-11-01 16:01:52 -0400

    thinking about architecture

--- src/Offline/ModelManager.java
+++ src/Offline/ModelManager.java
@@ -1,6 +1,6 @@
 package Offline;
 /**
- * Model manager is the overall interface for interaction.
+ * Model manager is managing models/sample in off line.
  * @author szhang
  *
  */
---
+++ src/Online/ELPDecider.java
@@ -0,0 +1,11 @@
+package Online;
+/**
+ * Deciding which model to use in Run-time.
+ * (Stole the name from BlinkDB).
+ * @author szhang
+ *
+ */
+
+public class ELPDecider {
+
+}
