18c5ca741693972538483958a8cfc2a1889bbde4
Shu Zhang <shu_zhang1989@yahoo.com.cn>
2013-11-04 19:08:25 -0500

    a

--- src/Model/Function.java
+++ src/Model/Function.java
@@ -3,6 +3,8 @@ package Model;
 public interface Function {
 	
 	//interface for storing the function pointer 
+	//Now we just assume typical cells can deduce to the value regardless
+	//of the impact of other rows.
 	int deduceValue(int[] inputValues);
 	
 }
