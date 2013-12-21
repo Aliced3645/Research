ad3df40db1371a398c6e24585c67b00b6d45b0cd
Shu <shu@shu-GE70-0NC.(none)>
2013-12-21 14:13:00 -0600

    working on weka-db

--- src/MySQL/WekaIntegrationTest.java
+++ src/MySQL/WekaIntegrationTest.java
@@ -1,9 +1,31 @@
 package MySQL;
 
+import weka.core.Instances;
+import weka.experiment.DatabaseUtils;
+import weka.experiment.InstanceQuery;
+
 /**
+ * Related Knowledge from: 
+ * 	http://weka.wikispaces.com/Databases
+ * 	http://weka.wikispaces.com/Use+Weka+in+your+Java+code
  * This class is for testing using Weka working with MySqL.
  * @author shu
  */
 public class WekaIntegrationTest {
+	
+	/**
+	 * Retrieving instances from MySQL database.
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception{
+		InstanceQuery query = new InstanceQuery();
+		query.setUsername("shu");
+		query.setPassword("shu");
+		query.setQuery("select * from table2");
+		Instances data = query.retrieveInstances();
+		System.out.println(data.get(1));
+	}
+	
 	
 }
